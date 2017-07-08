429592cb23c8431248c518015a3951d6105e3c05
Liz Couture <liz.a.couture@gmail.com>
2017-07-08 17:34:01 -0400

    Initialized Project

---
+++ Assets/Prompts.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prompts : MonoBehaviour {
+
+	public AudioClip[] promptVocals;
+	public AudioClip greeting;
+	public static AudioSource audio;
+
+	void Start(){
+	 audio = GetComponent<AudioSource>();
+	}
+
+	public void playRandomClip(){
+		audio.clip = promptVocals[Random.Range(0,promptVocals.Length)];
+		audio.Play();
+	}
+
+	public void playGreeting(){
+		audio.clip = greeting;
+		audio.Play();
+	}
+}
